0c4b45101cc3f576e76fb37d4cc5656d77a07b35
Tamara Simakova <tsimakova@parseq.pro>
2022-06-01 01:23:04 +0300

    Task-35_DB + Auth

--- unit_two/homework/task35/task35.py
+++ unit_two/homework/task35/task35.py
@@ -1,72 +1,66 @@
 import psycopg2
 
-class Database:
+class DB:
     def __init__(self, db_name, db_user, db_password):
         self.db_name = db_name
         self.db_user = db_user
         self.db_password = db_password
     def getConnection(self):
-        connection = psycopg2.connect(dbname = self.db_name, user = self.db_user, password =self.db_password)
+        return psycopg2.connect(dbname = self.db_name, user = self.db_user, password =self.db_password)
+
+class Auth(DB):
+    auth_status = False
+    def __init__(self):
+        pass
+
+    def registration(self):
+        id_group = str(input("Введите номер группы: "))
+        name = str(input("Введите имя: "))
+        surname = str(input("Введите фамилию: "))
+        patronym = str(input("Введите отчество: "))
+        age = str(input("Введите возраст: "))
+        login = str(input("Введите логин: "))
+        password = str(input("Введите пароль: "))
+        connection = self.getConnection()
         cursor = connection.cursor()
-        return cursor
+        cursor.execute("select id_profile from profile")
+        users = cursor.fetchall()
+        id_new = len(users) + 1
+        cursor.execute("insert into profile (id_profile, id_group, name, surname, patronym, age, login, password) values (%s, %s, %s, %s, %s, %s, %s, %s)", [str(id_new), id_group, name, surname, patronym, age, login, password])
+        print('Поздравляю, вы успешно зарегистрированы!\nВаши учетные данные:')
+        connection.commit()
+        cursor.execute("select * from profile where id_profile = %s", [id_new])
+        new_user = cursor.fetchall()
+        return new_user[0]
+        cursor.close()
+        connection.close()
 
-class Authentification:
-    def __init__(self, user_login, user_password, cursor):
-        self.user_login = user_login
-        self.user_password = user_password
-        self.cursor = cursor
     def login(self):
-        auth_status = False
-        self.user_login = str(input("Введите имя пользователя: "))
-        self.user_password = str(input("Введите пароль : "))
-        self.cursor.execute("select id_student from credential where login like %s and password like %s", [self.user_login, self.user_password])
-        id_student = cursor.fetchone()
-        if id_student is not None:
-            self.auth_status = True
-            return (id_student[0], auth_status)
+        user_login = str(input("Введите имя пользователя: "))
+        user_password = str(input("Введите пароль : "))
+        connection = self.getConnection()
+        cursor = connection.cursor()
+        cursor.execute("select * from profile where login like %s and password like %s", [user_login, user_password])
+        user = cursor.fetchall()
+        if cursor.rowcount == 1:
+            auth_status = True
+            welcome = "Добро пожаловать, " + str(user[0][2] + " " + str(user[0][4]) + "!")
+            return welcome
         else:
-            self.auth_status = False
-            return ("Unrecognized", auth_status)
-    def createUser(self):
-        auth_status = False
-        lgn = str(input('Введите имя пользователя:'))
-        psw = str(input('Введите пароль:'))
-        cursor.execute("select id_student from student")
-        ids = cursor.fetchall()
-        id_new = (len(ids)) + 1
-        self.cursor.execute("insert into credential values (%s, %s, %s, %s)", [str(id_new), lgn, psw, str(id_new)])
-        name = str(input('Введите свое имя:'))
-        surname = str(input('Введите свою фамилию:'))
-        patronym = str(input('Введите свое отчество:'))
-        age = int(input('Введите свой возраст:'))
-        sex = 'True'
-        group = str(input('Введите номер группы:'))
+            Auth.auth_status = False
+            deny = "Пользователь не найден. Проверьте правильность введенных учетных данных."
+            return deny
 
-        pass
     def logout(self):
-        pass
+        Auth.auth_status = False
 
 
-# Start
 
-#action = input("Выберите действие:\n"
- #              "1. Войти.\n"
-  #             "2. Зарегистрироваться.\n")
-
-
-action = 1
-#action = 2
-
-if action == 1:
-    access = Database("TestSystem", "postgres", "123")
-    cursor = access.getConnection()
-    creds = Authentification('eric_27', '123', cursor)
-    idn = creds.login()
-    print(idn)
-elif action == 2:
-    pass
-else:
-    pass
+access = DB("TestSystem", "postgres", "123")
+connection = access.getConnection()
+#user = Auth.login(access)
+new_user = Auth.registration(access)
+print(new_user)
 
 
 
@@ -88,4 +82,32 @@ class Test:
 
 
 
+# 27.05.22
+
+# class View:
+#     def render(self):
+#         pass
+#
+# class TestsView(View):
+#     def __init__(self, template, data):
+#         self.template =
+#         self.data  = data
+#         pass
+#     def render(self):
+#         # Логика вывода тестов в консоль
+#         cursor.execute("select * from test")
+#         data = cursor.fetchall()
+#         tests_view = template(data)
+#         print(tests_view)
+#
+# class QuestionView(View):
+#     def __init__(self, id_test, theme):
+#         self.id_test = id_test
+#         self.theme = theme
+#     def render(self):
+#         # Логика вывода вопросов в консоль
+#         cursor.execute("select * from question")
+#         data = cursor.fetchall()
+#         question_view = template(data)
+#         print(tests_view)
 
